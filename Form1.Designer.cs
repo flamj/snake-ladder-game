ecdce3db6b0ea4aaf344833704750f5e6b9e3e59
flamj <68989789+flamj@users.noreply.github.com>
2020-10-29 15:31:18 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SnakeLadder
+{
+    partial class snake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(snake));
+            this.display = new System.Windows.Forms.RichTextBox();
+            this.roll = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.about = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.BackColor = System.Drawing.Color.Teal;
+            this.display.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display.ForeColor = System.Drawing.SystemColors.Info;
+            this.display.Location = new System.Drawing.Point(1, 102);
+            this.display.Margin = new System.Windows.Forms.Padding(0);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(426, 348);
+            this.display.TabIndex = 0;
+            this.display.Text = "";
+            this.display.TextChanged += new System.EventHandler(this.display_TextChanged);
+            // 
+            // roll
+            // 
+            this.roll.BackColor = System.Drawing.Color.Transparent;
+            this.roll.Font = new System.Drawing.Font("Ravie", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roll.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.roll.Location = new System.Drawing.Point(471, 189);
+            this.roll.Name = "roll";
+            this.roll.Size = new System.Drawing.Size(101, 63);
+            this.roll.TabIndex = 1;
+            this.roll.Text = "Roll";
+            this.roll.UseVisualStyleBackColor = false;
+            this.roll.Click += new System.EventHandler(this.roll_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.about,
+            this.newGameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(613, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // about
+            // 
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(62, 24);
+            this.about.Text = "About";
+            this.about.Click += new System.EventHandler(this.about_Click);
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(124, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 38);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Snake Ladder Game";
+            // 
+            // snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(613, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.roll);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "snake";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake Ladder Game";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox display;
+        private System.Windows.Forms.Button roll;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem about;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeLadder
+{
+    public partial class snake : Form
+    {
+        int position = 0;
+        int newposition = 0;
+        
+
+        public snake()
+        {
+            InitializeComponent();
+        }
+
+        private void about_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This game will generate the condition on the text box. \n Players are only require to click the \"Roll\" button", "About");
+        }
+
+        private void display_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void roll_Click(object sender, EventArgs e)
+        {
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+
+            newposition = position + roll;
+            String con = condition(newposition);
+            newposition = ladder(newposition);
+            newposition = eaten(newposition);
+           
+            if(newposition >= 100)
+            {
+                display.AppendText(position + " rolled " + roll + " " + con + " = position 100" + "\n");
+                MessageBox.Show("You Won!", "Message");
+            }
+            else
+            {
+                display.AppendText(position + " rolled " + roll + " " + con + " = position " + newposition + "\n");
+                position = newposition;
+            }
+        }
+
+        private String condition(int value)
+        { 
+            if (value == 3 || value == 6 || value == 20 || value == 36 || value == 63 || value == 68)
+            {
+                return "Ladder is used";
+
+            } else if (value == 25 || value == 33 || value == 47 || value == 65 || value == 87 || value == 91 || value == 99)
+            {
+                return "Eaten by snake";
+            }
+            return "";
+        }
+
+        private int ladder (int value)
+        {
+            switch (value)
+            {
+                case 3: return 51;
+
+                case 6: return 27;
+
+                case 20: return 60;
+
+                case 36: return 55;
+
+                case 63: return 95;
+
+                case 68: return 98;
+
+            }
+
+            return value;
+
+        }
+
+        private int eaten(int value)
+        {
+            switch (value)
+            {
+                case 91: return 61;
+
+                case 87: return 57;
+
+                case 99: return 69;
+
+                case 65: return 52;
+
+                case 47: return 19;
+
+                case 25: return 5;
+
+                case 33: return 1;
+
+            }
+
+            return value;
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            display.Text = "";
+            position = 0;
+            newposition = 0;
+        }
+    }
+}
